1196ddc8a6f6c6cc63e4c38fb5b9d2a29d095f7b
Samir <khadkasamir8079@gmail.com>
2025-02-01 11:04:00 +0545

    complete courses Section

--- Desktop/Edu-connect/client/src/components/student/CoursesSection.jsx
+++ Desktop/Edu-connect/client/src/components/student/CoursesSection.jsx
@@ -8,7 +8,7 @@ const CoursesSection = () => {
   return (
     <div className='py-16 md:px-40 px-8'>
       <h2 className='text-3xl font-medium text-gray-800"'>Learn from the best</h2>
-      <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to business and 
+      <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to <br/> business and 
       wellness, our courses are crafted to deliver results.</p>
 
       <div className='grid grid-cols-4 px-4 md:px-0 md:my-16 my-10 gap-4'>
--- Desktop/Edu-connect/client/src/context/AppContext.jsx
+++ Desktop/Edu-connect/client/src/context/AppContext.jsx
@@ -1,9 +1,11 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 export const AppContext = createContext()
 export const AppContextProvider = (props)=>{
 
     const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
 
     const [allCourses, setAllCourses] = useState([])
 
@@ -12,6 +14,17 @@ export const AppContextProvider = (props)=>{
         setAllCourses(dummyCourses)
     }
     
+    // Function to calculate average rating of course
+    const calculateRating = (course)=>{
+        if(course.courseRatings.length ===0){
+            return 0;
+        }
+        let totalRating = 0
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        })
+        return totalRating / course.courseRatings.length
+    }
     useEffect(()=>{
         fetchAllCourses()
     },[])
@@ -19,7 +32,7 @@ export const AppContextProvider = (props)=>{
 
 
     const value = {
-        currency,allCourses
+        currency,allCourses,navigate,calculateRating
     }
 
     return (
