ccee9a69fbcf7d3e19adf811698db56af8159688
Samir <khadkasamir8079@gmail.com>
2025-02-01 10:11:31 +0545

    change on course card

--- Desktop/Edu-connect/client/src/components/student/CoursesSection.jsx
+++ Desktop/Edu-connect/client/src/components/student/CoursesSection.jsx
@@ -11,7 +11,7 @@ const CoursesSection = () => {
       <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to business and 
       wellness, our courses are crafted to deliver results.</p>
 
-      <div>
+      <div className='grid grid-cols-4 px-4 md:px-0 md:my-16 my-10 gap-4'>
       {allCourses.slice(0,4).map((course, index)=> <CourseCard key={index} course={course}/>)}
       </div>
 
