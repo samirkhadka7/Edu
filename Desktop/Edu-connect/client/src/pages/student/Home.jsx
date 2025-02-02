3c4d53f1ca8b2b1e0897829002e378ce8ddd76c2
Samir <khadkasamir8079@gmail.com>
2025-02-02 08:17:06 +0545

    complete Call to Action

--- Desktop/Edu-connect/client/src/pages/student/Home.jsx
+++ Desktop/Edu-connect/client/src/pages/student/Home.jsx
@@ -3,6 +3,8 @@ import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CoursesSection from '../../components/student/CoursesSection'
 import TestiMonialsSection from '../../components/student/TestimonialsSection'
+import CallToAction from '../../components/student/CallToAction'
+import Footer from '../../components/student/Footer'
 
 
 const Home = () => {
@@ -13,6 +15,8 @@ const Home = () => {
       <Companies/>
       <CoursesSection/>
       <TestiMonialsSection />
+      <CallToAction/>
+      <Footer />
     </div>
   )
 }
