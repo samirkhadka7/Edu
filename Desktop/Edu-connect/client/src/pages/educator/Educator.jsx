5918f572eb48972047e4e7ce0cb607943b862d5e
Samir <khadkasamir8079@gmail.com>
2025-02-10 17:55:25 +0545

    complete side bar

--- Desktop/Edu-connect/client/src/pages/educator/Educator.jsx
+++ Desktop/Edu-connect/client/src/pages/educator/Educator.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../../components/educator/Sidebar'
+import Footer from '../../components/educator/Footer'
+// import Navbar from '../../components/student/Navbar'
 const Educator = () => {
   return (
-    <div>
-      <h1>Educator</h1>
-      <div>
-        {<Outlet/>}
+    <div className='text-default min-h-screen bg-white' >
+      {/* < Navbar /> */}
+      <div className='flex' >
+        <Sidebar />
+        <div className='flex-1'>
+          {<Outlet/>}
+        </div>
       </div>
+      <Footer />
     </div>
+    
   )
 }
 
