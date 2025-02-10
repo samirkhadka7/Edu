d9e20c8a11ba1d4a7be2b279cb16b120838a533e
Samir <khadkasamir8079@gmail.com>
2025-02-10 18:14:45 +0545

    work on dashboard

--- Desktop/Edu-connect/client/src/pages/educator/Dashboard.jsx
+++ Desktop/Edu-connect/client/src/pages/educator/Dashboard.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { assets, dummyDashboardData } from '../../assets/assets'
+import { AppContext } from '../../context/AppContext'
+import Loading from '../../components/student/Loading'
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h1>DashBoard</h1>
+  const { currency } = useContext(AppContext)
+  const [dashboardData, setDashboardData] = useState(null)
+
+  const fetchDashboardData = async ()=> {
+    setDashboardData (dummyDashboardData)
+  }
+
+  useEffect (() => {
+    fetchDashboardData ()
+  }, [])
+
+  return dashboardData ? (
+    <div className='min-h-screen flex flex-col items-start justify-between gap-8
+    md: p-8 md:pb-0p-4 pt-8 pb-0'>
+      <div className='space-y-5'>
+        <div className='flex flex-wrap gap-5 items-center'>
+          <div className='flex items-center gap-3 shadow-card border border-blue-500
+          p-4 w-56 rounded-md'>
+            <img src={assets.patients_icon} alt="patients_icon" />
+            <div>
+            <p className='text-2x] font-medium text-gray-600'>{dashboardData.enrolledStudentsData.length}</p>
+            <p className='text-base text-gray-500'>Total Enrolments</p>
+            </div>
+          </div>
+
+          <div className='flex items-center gap-3 shadow-card border border-blue-500
+          p-4 w-56 rounded-md'>
+            <img src={assets.appointments_icon} alt="patients_icon" />
+            <div>
+            <p className='text-2x] font-medium text-gray-600'>{dashboardData.totalcourses}</p>
+            <p className='text-base text-gray-500'>Total Courses</p>
+            </div>
+          </div>
+
+          <div className='flex items-center gap-3 shadow-card border border-blue-500
+          p-4 w-56 rounded-md'>
+            <img src={assets.earning_icon} alt="patients_icon" />
+            <div>
+            <p className='text-2x] font-medium text-gray-600'>{currency}{dashboardData.totalEarnings}</p>
+            <p className='text-base text-gray-500'>Total Earnings</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
-  )
+    
+  ): <Loading />
 }
 
 export default Dashboard
