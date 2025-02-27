acd8ca1134bc2460ccba3b116735243946f62845
Samir <khadkasamir8079@gmail.com>
2025-02-27 15:26:15 +0545

    update on footer

--- Desktop/Edu-connect/client/src/components/Footer.jsx
+++ Desktop/Edu-connect/client/src/components/Footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
-
-
+const Footer = () => {
+  return (
+    <div className='bg-gray-800 py-4'>
+      <p className='text-white text-center'>© 2024 LMS Platform. All Rights Reserved.</p>
+    </div>
+  )
+}
 export default Footer
